218ce6298d27340fb629f095bede7ad056758ee9
chandan1203 <chandan.aece.iu@gmail.com>
2022-07-20 17:25:19 +0600

    product created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ProductCreated;
+use App\Jobs\ProductDeleted;
+use App\Jobs\ProductUpdated;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,15 +26,20 @@ class ProductController extends Controller
         // return $request->only('title', 'image');
         $products = Product::create($request->only('title', 'image'));
 
+        ProductCreated::dispatch($products->toArray());
+
         return response($products, Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id)
     {
+        // return $request->all();
         // return $request->only('title', 'image');
         $product = Product::findOrFail($id);
 
         $product->update($request->only('title', 'image'));
+
+        ProductUpdated::dispatch($product->toArray());
 
         return response($product, Response::HTTP_ACCEPTED);
     }
@@ -39,6 +47,8 @@ class ProductController extends Controller
     public function destroy($id)
     {
         Product::find($id)->delete();
+
+        ProductDeleted::dispatch($id);
 
         return response(null, Response::HTTP_NO_CONTENT);
 
